53773fc563f7925138620e231eec6b239c9b64ea
Semen <senia-2010@mail.ru>
2022-06-13 22:38:09 +0300

    add movies

---
+++ src/components/Movie/Movie.js
@@ -0,0 +1,11 @@
+import './Movie.css';
+
+function Movie() {
+    return (
+        <>
+            <p>Netflix Movie</p>
+        </>
+    );
+}
+
+export default Movie;
---
+++ src/components/Movies/Movies.js
@@ -0,0 +1,11 @@
+import './Movies.css';
+
+function Movies() {
+    return (
+        <>
+            <p>Netflix Movies</p>
+        </>
+    );
+}
+
+export default Movies;
